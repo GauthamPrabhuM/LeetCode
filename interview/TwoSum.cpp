5f4981cb5d9c557c5227fe8ccce12a1aa50bba3a
Gautham Prabhu M <71874814+GauthamPrabhuM@users.noreply.github.com>
2021-10-04 16:06:18 +0530

    Create TwoSum.cpp
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.

---
+++ interview/TwoSum.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        unordered_map<int,int> map;
+        vector<int> v;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(map.find(target-nums[i])!=map.end())
+            {
+                v.push_back(map[target-nums[i]]);
+                v.push_back(i);
+                return v;
+            }
+            else
+                map[nums[i]]=i;
+        }
+        return v;
+    }
+};
+
+/* 
