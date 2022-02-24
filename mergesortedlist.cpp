f85d7f38a23db7b465e801bbb900a8e62a24d2d2
Gautham Prabhu M <71874814+GauthamPrabhuM@users.noreply.github.com>
2022-02-24 22:02:03 +0530

    Create mergesortedlist.cpp

---
+++ mergesortedlist.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    ListNode* merge(ListNode* l1, ListNode* l2)
+    {
+        if(!l1)
+            return l2;
+        if(!l2)
+            return l1;
+        if(l1->val<l2->val)
+        {
+            l1->next=merge(l1->next,l2);
+            return l1;
+        }
+        else
+        {
+            l2->next=merge(l1,l2->next);
+            return l2;
+        }
+    }
+    ListNode* sortList(ListNode* head) 
+    {
+      if(!head or !head->next)
+            return head;
+      ListNode *slow=head, *fast=head->next;
+      while(fast && fast->next)
+      {
+          slow=slow->next;
+          fast=fast->next->next;
+      }
+      ListNode *newHead=slow->next;
+        slow->next=nullptr;
+        return merge(sortList(head),sortList(newHead));
+        
+    }
+};
